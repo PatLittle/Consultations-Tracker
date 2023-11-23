37dbf4272de9ec352747092cc6b9eb5db178717c
Patrick Little <31454591+PatLittle@users.noreply.github.com>
2023-11-23 18:31:06 -0500

    Create get-consulations.py

---
+++ get-consulations.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import yaml
+
+# Path to the uploaded YAML file
+yaml_file_path = '.upptimerc.yml'  # Replace with your actual YAML file path
+
+# URL of the CSV file
+csv_url = 'https://open.canada.ca/data/en/datastore/dump/92bec4b7-6feb-4215-a5f7-61da342b2354?bom=True'  # Replace with the actual URL of the CSV file
+
+# Read the existing YAML file
+with open(yaml_file_path, 'r', encoding='utf8') as file:
+    yaml_content = yaml.safe_load(file)
+
+# Read the CSV file
+df = pd.read_csv(csv_url)
+
+# Select specific columns and rename them for YAML
+selected_data = df[['title_en', 'profile_page_en']].rename(columns={'title_en': 'name', 'profile_page_en': 'url'})
+
+# Update the 'sites' section in the YAML content
+yaml_content['sites'] = selected_data.to_dict(orient='records')
+
+# Save the updated content back to the YAML file
+with open(yaml_file_path, 'w', encoding='utf8') as file:
+    yaml.dump(yaml_content, file, sort_keys=False, allow_unicode=True)
+
+print("YAML file updated successfully.")
